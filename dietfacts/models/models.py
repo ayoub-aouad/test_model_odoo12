ef0130fe6fa783c505101304ec632b3fde195e29
ayoub-aouad <59152669+ayoub-aouad@users.noreply.github.com>
2020-08-13 01:03:28 +0200

    day6

--- dietfacts/models/models.py
+++ dietfacts/models/models.py
@@ -2,11 +2,20 @@
 
 from odoo import models, fields, api
 
-class dietfacts(models.Model):
+class dietfacts_product_template(models.Model):#this module is an inherit model that add and modify in a existing model
     _name = 'product.template'
     _inherit = ["product.template"]
-    mx_calories = fields.Integer(string="Calories", required=False, )
-    mx_protain = fields.Float(string="Protain",  required=False, )
-    mx_text = fields.Text(string="Text", required=False, )
+    _description = "MX_inherit_MODEL1"
+    mx_calories = fields.Integer(string="Mx_Calories", required=False, )
+    mx_protain = fields.Float(string="Mx_Protain",  required=False, )
+    mx_text = fields.Text(string="Mx_Text", required=False, )
+    #mx_dietitem= fields.Boolean(string="Mx_dietitem",  )
+class dietfacts_res_users_info(models.Model):
+    _name = 'mx.res.users'
 
+    _description = 'MX_res_Model'
 
+    mx_name = fields.Char(string="Mx_Users", required=False, )
+    mx_datetime = fields.Datetime(string="Mx_DateTime", required=True, )
+    mx_users_id = fields.Many2one(comodel_name="res.users", string="Mx_Users_Id", required=False, )
+    mx_comment = fields.Text(string="Mx_Comment", required=False, )
